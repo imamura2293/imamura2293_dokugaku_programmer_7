63da88f8f3e692b768ea8628e76b349444edbe53
pana2 <imamura2293@gmail.com>
2020-09-06 23:51:26 +0900

    dai6syou

---
+++ dokugaku7.py
@@ -0,0 +1,79 @@
+"""line one
+    line two
+    line three
+"""
+
+author = "Kafka"
+author[0]
+author[1]
+author[2]
+author[3]
+author [4]
+
+
+author = "Kafka"
+author[5]
+
+author = "Kafka"
+author(-1)
+
+
+author = "Kafka"
+author[-2]
+author[-3]
+
+
+
+ff = "F. Fitzgerald"
+ff = "F. Scott Fitzgerald"
+ff
+
+
+"cat" + "in" + "hat"
+
+
+
+"cat" + " in" + " the" + "hat"
+
+
+"Sawyer" * 3
+
+
+"We hold these truths...".upper()
+
+
+"SO IT GOES.".lower()
+
+
+"four score and...".capitalize()
+
+こんにちは、{}".format("ウィリアム・フォークナー")
+
+
+
+name = "ウィリアム・フォークナー・
+"こんにちは、{}".format (name)
+>
+
+
+author = "ウィリアム・フォークナー"
+year_born = "1897"
+6
+"{}は{}年に生まれました。".format (author, year_born)
+
+
+
+what = input("何が:")
+when = input("いつ:")
+where = input("どこで:")
+do = input("どうした:")
+
+r = "{}は{}に{}で{}。".format(what, when, where, do)
+print (r)
+
+
+
+
+
+
+
